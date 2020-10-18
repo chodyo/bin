30ee1dc51a0bed6700d59b2e63db79b635025212
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2020-10-18 18:09:28 -0400

    Disable Motion when someone is home

---
+++ monitor_while_away.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# usage: monitor_while_away.py
+
+import requests, sys, syslog, time
+from is_home import *
+
+URL = MOTION_URL_HERE
+SLEEP = 5 # seconds
+AWAY_THRESHOLD = 300 # seconds
+
+def main():
+        while True:
+                try:
+                        time.sleep(SLEEP) # give it a few seconds to come up
+                        monitor()
+                except Exception as e:
+                        syslog.syslog(syslog.LOG_ERR, f'problem with monitor_while_away.py err={e}')
+
+def monitor():
+        # motion comes up active and gets set to pause as soon as one person is home.
+        # this guarantees it stays active as long as no one is home.
+        last_home_time = time.time() - (AWAY_THRESHOLD + 1)
+
+        while True:
+                if is_home(["anyone"]):
+                        last_home_time = time.time()
+                syslog.syslog(syslog.LOG_DEBUG, f"the last time someone was home was {time.strftime('%D %H:%M:%S', time.localtime(last_home_time))}")
+                active = motion_active()
+                syslog.syslog(syslog.LOG_DEBUG, f"motion active={active}")
+
+                if recently_home(last_home_time) and active:
+                        syslog.syslog(syslog.LOG_INFO, "someone is home, pausing motion")
+                        requests.get(URL+"pause")
+                elif not recently_home(last_home_time) and not active:
+                        syslog.syslog(syslog.LOG_INFO, "no one is home, starting motion")
+                        command_motion(URL+"start")
+
+                time.sleep(SLEEP)
+
+def recently_home(last_home_time):
+        return (time.time() - last_home_time) < AWAY_THRESHOLD
+
+def motion_active():
+        resp = requests.get(url=URL+"status")
+        syslog.syslog(syslog.LOG_DEBUG, f"motion responded to status request with {resp.text}")
+        if "Detection status ACTIVE" in resp.text:
+                return True
+        return False
+
+def command_motion(command):
+        resp = requests.get(url=URL+command)
+
+if __name__ == '__main__':
+        main()
